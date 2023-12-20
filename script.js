941205401b1682d91729f8d8775fdab6c59fc305
zijian-0712 <153837594+zijian-0712@users.noreply.github.com>
2023-12-20 20:18:40 +0800

    modified:   index.html 	new file:   public/images/AIBank.jpg 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -10,3 +10,19 @@ document.getElementById('pre').onclick = function () {
     let lists = document.querySelectorAll('.item');
     document.getElementById('slide').prepend(lists[lists.length-1]);
 }
+
+
+let lists01 = document.querySelectorAll('.item');
+    if (lists01.onclick==true) {
+        lists01[2].onclick = function ()
+        {
+        let lists = document.querySelectorAll('.item');
+        document.getElementById('slide').appendChild(lists[0]);
+        lists01 = document.querySelectorAll('.item');
+        }
+    
+    }
+
+   
+
+
